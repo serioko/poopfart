2e8029dfebd5c13a8e335c765fe75b80fc60236a
serioko <seriokomb@gmail.com>
2017-04-05 18:37:51 -0400

    initial app and notes file

---
+++ hello.py
@@ -0,0 +1,43 @@
+from flask import Flask
+app = Flask(__name__)
+import enchant
+import random
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/dictionary/<myword>')
+def show_user_dict(myword):
+    # show the user profile for that user
+    d = enchant.request_dict("en_US")
+    my = d.suggest(myword)
+     
+    return 'suggestions {}'.format(my)
+
+
+@app.route("/")
+def hello():
+    import enchant
+
+    my_unused_variable = """<html>
+    <body>
+    <div style="background:red">
+    <h1>WHATUP </h1>
+    </div>
+    <div style="background:#CCF">
+    <h1>falafael </h1>
+    </div>
+    </body>
+    </html>"""
+
+    myhtmlstring = "poop"
+    for poop in range(random.randint(1,10)):
+        myhtmlstring = myhtmlstring + str(poop)
+
+    return myhtmlstring
+
+if __name__ == "__main__":
+    app.run()
---
+++ notes.md
@@ -0,0 +1,6 @@
+pip is an extractor that grabs stuff for you when you tell it to?
+visual studio code is a code writing program
+python is a programming language
+we use pip (python package management system) to download librairies/packages to use in our python programs
+we write these programs in vscode
+we use cmd as a direct interface into using software, instead of clicking around a bunch
